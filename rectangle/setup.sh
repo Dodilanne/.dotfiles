750672fe47c1b15643de96e13caa360df70af7fb
Dodilanne <vincaudet@gmail.com>
2023-08-08 09:50:13 -0400

    Add more configs

---
+++ rectangle/setup.sh
@@ -0,0 +1,17 @@
+defaults write com.knollsoft.Rectangle specified -dict-add keyCode -float 34 modifierFlags -float 917795
+defaults write com.knollsoft.Rectangle specifiedHeight -float 0.5
+defaults write com.knollsoft.Rectangle specifiedWidth -float 0.5
+
+defaults write com.knollsoft.Rectangle terminal -dict-add keyCode -float 4 modifierFlags -float 917795
+defaults write com.knollsoft.Rectangle terminalHeight -float 0.8
+defaults write com.knollsoft.Rectangle terminalWidth -float 0.5
+
+defaults write com.knollsoft.Rectangle screenEdgeGapTop -int 10
+defaults write com.knollsoft.Rectangle screenEdgeGapBottom -int 10
+defaults write com.knollsoft.Rectangle screenEdgeGapLeft -int 10
+defaults write com.knollsoft.Rectangle screenEdgeGapRight -int 10
+
+defaults write com.knollsoft.Rectangle snapEdgeMarginTop -int 10
+defaults write com.knollsoft.Rectangle snapEdgeMarginBottom -int 10
+defaults write com.knollsoft.Rectangle snapEdgeMarginLeft -int 10
+defaults write com.knollsoft.Rectangle snapEdgeMarginRight -int 10
---
+++ skhd/rc.sh
@@ -0,0 +1,15 @@
+alt + shift + ctrl - f : open -a "Finder"
+alt + shift + ctrl - p : open -a "Postman"
+alt + shift + ctrl - b : open -a "Google Chrome"
+
+alt + shift + ctrl - a : open -a "Discord"
+alt + shift + ctrl - r : open -a "Spotify"
+alt + shift + ctrl - s : open -a "Slack"
+alt + shift + ctrl - t : open -a "iTerm"
+alt + shift + ctrl - g : open -a "GitKraken"
+
+alt + shift + ctrl - z : open -a "Messenger"
+alt + shift + ctrl - x : open -a "Messages"
+alt + shift + ctrl - c : open -a "Safari"
+alt + shift + ctrl - d : open -a "DataGrip"
+alt + shift + ctrl - v : open -a "Logic Pro X"
