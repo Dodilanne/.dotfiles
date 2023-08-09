d314c5b7d57c5ac3475f90bb7652eb010bb2811b
Dodilanne <vincaudet@gmail.com>
2023-08-09 16:38:55 -0400

    Update theme

--- iterm/auto_dark_mode.py
+++ iterm/auto_dark_mode.py
@@ -4,7 +4,7 @@ import iterm2
 
 
 async def changeTheme(connection, parts):
-    theme_dark = "kanagawa_dragon"
+    theme_dark = "rose-pine"
     theme_light = "rose-pine-dawn"
 
     wallpapers_path = "/Users/dodi/Library/CloudStorage/OneDrive-Personal/Photos/Wallpapers/iterm"
