628777c22d44ce333f61f2f1b555cca28b552d11
Dodilanne <dodilanne@ada-anvil.io>
2023-08-16 09:59:24 -0400

    Fix

--- rectangle/setup.sh
+++ rectangle/setup.sh
@@ -1,10 +1,6 @@
-defaults write com.knollsoft.Rectangle specified -dict-add keyCode -float 34 modifierFlags -float 917795
+defaults write com.knollsoft.Rectangle specified -dict-add keyCode -float 32 modifierFlags -float 917795
 defaults write com.knollsoft.Rectangle specifiedHeight -float 0.5
 defaults write com.knollsoft.Rectangle specifiedWidth -float 0.5
-
-defaults write com.knollsoft.Rectangle terminal -dict-add keyCode -float 4 modifierFlags -float 917795
-defaults write com.knollsoft.Rectangle terminalHeight -float 0.8
-defaults write com.knollsoft.Rectangle terminalWidth -float 0.5
 
 defaults write com.knollsoft.Rectangle screenEdgeGapTop -int 10
 defaults write com.knollsoft.Rectangle screenEdgeGapBottom -int 10
