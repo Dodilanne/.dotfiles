c0f4fdeb86ce5faac19b6a95396439d4b3938351
Dodilanne <dodilanne@ada-anvil.io>
2023-10-09 12:20:17 -0400

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,24 +9,24 @@ alias lt="exa --tree --long"
 alias ll="exa --long"
 alias v=vim
 alias n=nvim
-alias cat=bat 
+alias cat=bat
 
 bindkey '^R' history-incremental-search-backward
 
 bindkey -s '^f' "tmux_sessionizer\n"
 
 take() {
-  mkdir -p $1
-  cd $1
+    mkdir -p $1
+    cd $1
 }
 
 # tmux
 tma() {
-  tmux attach -t $1
+    tmux attach -t $1
 }
 
 tmn() {
-  tmux new -s $1
+    tmux new -s $1
 }
 
 # colors
@@ -34,25 +34,24 @@ DARK_COLOR="base16-rose-pine.yml"
 LIGHT_COLOR="base16-rose-pine-dawn.yml"
 
 day() {
-  export NEOVIM_BACKGROUND="light"
-  alacritty-colorscheme -V apply $LIGHT_COLOR
-  dark-mode off
-  spicetify config color_scheme rose-pine-dawn
-  spicetify apply -y
+    export NEOVIM_BACKGROUND="light"
+    alacritty-colorscheme -V apply $LIGHT_COLOR
+    dark-mode off
+    spicetify config color_scheme rose-pine-dawn
+    spicetify apply -y
 }
 
 night() {
-  export NEOVIM_BACKGROUND="dark"
-  alacritty-colorscheme -V apply $DARK_COLOR
-  dark-mode on
-  spicetify config color_scheme rose-pine
-  spicetify apply -y
+    export NEOVIM_BACKGROUND="dark"
+    alacritty-colorscheme -V apply $DARK_COLOR
+    dark-mode on
+    spicetify config color_scheme rose-pine
+    spicetify apply -y
 }
 
 # Initialize neovim background flag
 if hash is_dark_mode 2>/dev/null && is_dark_mode; then
-  export NEOVIM_BACKGROUND="dark"
+    export NEOVIM_BACKGROUND="dark"
 else
-  export NEOVIM_BACKGROUND="light"
+    export NEOVIM_BACKGROUND="light"
 fi
-
