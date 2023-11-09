b58b5c0b70e98a29ac2b985340461652b6e462a2
Dodilanne <dodilanne@ada-anvil.io>
2023-11-09 09:26:43 -0500

    Change theme

--- iterm/auto_dark_mode.py
+++ iterm/auto_dark_mode.py
@@ -4,8 +4,8 @@ import iterm2
 
 
 async def changeTheme(connection, parts):
-    theme_dark = "rose-pine"
-    theme_light = "rose-pine-dawn"
+    theme_dark = "kanagawa_dragon"
+    theme_light = "kanagawa_lotus"
 
     wallpapers_path = (
         "/Users/dodi/Library/CloudStorage/OneDrive-Personal/Photos/Wallpapers/iterm"
--- skhd/rc.sh
+++ skhd/rc.sh
@@ -1,14 +1,13 @@
-alt + shift + ctrl - f : open -a "Finder"
+alt + shift + ctrl - w : open -a "Finder"
 alt + shift + ctrl - p : open -a "Postman"
-alt + shift + ctrl - b : open -a "Google Chrome"
+alt + shift + ctrl - c : open -a "Google Chrome"
 
 alt + shift + ctrl - r : open -a "Spotify"
 alt + shift + ctrl - s : open -a "Slack"
-alt + shift + ctrl - t : open -a "Alacritty"
+alt + shift + ctrl - f : open -a "iTerm"
 alt + shift + ctrl - g : open -a "GitKraken"
 
 alt + shift + ctrl - z : open -a "Messenger"
 alt + shift + ctrl - x : open -a "Messages"
-alt + shift + ctrl - c : open -a "Safari"
+alt + shift + ctrl - b : open -a "Safari"
 alt + shift + ctrl - d : open -a "DataGrip"
-alt + shift + ctrl - v : open -a "Logic Pro X"
