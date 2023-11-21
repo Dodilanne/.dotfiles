29eb3ad12f70aefb88eb0ebd6e13f0ab591a6c33
Dodilanne <vincaudet@gmail.com>
2023-11-21 07:21:05 -0500

    Change spotify to music app shortcut

--- skhd/rc.sh
+++ skhd/rc.sh
@@ -3,7 +3,7 @@ alt + shift + ctrl - p : open -a "Postman"
 alt + shift + ctrl - c : open -a "Google Chrome"
 alt + shift + ctrl - n : open -a "Obsidian"
 
-alt + shift + ctrl - r : open -a "Spotify"
+alt + shift + ctrl - r : open -a "Music"
 alt + shift + ctrl - s : open -a "Slack"
 alt + shift + ctrl - f : open -a "iTerm"
 alt + shift + ctrl - g : open -a "GitKraken"
