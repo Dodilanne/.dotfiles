0e74010fd8883b2c047403b207f4b0c940aba7f5
Dodilanne <vincaudet@gmail.com>
2024-07-08 19:15:15 -0400

    New zsh config

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,116 +1,115 @@
-source_if_exists () {
-    if test -r "$1"; then
-        source "$1"
-    fi
-}
-
-source_if_exists $HOME/.env.sh
-source_if_exists $DOTFILES/zsh/history.zsh
-source_if_exists $DOTFILES/zsh/git.zsh
-source_if_exists $DOTFILES/zsh/aliases.zsh
-source_if_exists /usr/local/etc/profile.d/z.sh
-source_if_exists /opt/homebrew/etc/profile.d/z.sh
-
-if type "direnv" > /dev/null; then
-    eval "$(direnv hook zsh)"
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-if type "thefuck" > /dev/null; then
-    eval "$(thefuck --alias)"
+if [[ -f "/opt/homebrew/bin/brew" ]] then
+  # If you're using macOS, you'll want this enabled
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-function zvm_after_init() {
-    source $DOTFILES/zsh/aliases.zsh
-}
+# Set the directory we want to store zinit and plugins
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 
-function zvm_config() {
-    # Disable the cursor style feature
-    ZVM_CURSOR_STYLE_ENABLED=false
-}
+# Download Zinit, if it's not there yet
+if [ ! -d "$ZINIT_HOME" ]; then
+   mkdir -p "$(dirname $ZINIT_HOME)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+# Source/Load zinit
+source "${ZINIT_HOME}/zinit.zsh"
+
+# Add in Powerlevel10k
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# Add in zsh plugins
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
+
+# Load completions
+autoload -Uz compinit && compinit
+
+zinit cdreplay -q
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+source $HOME/.env.sh
 
 export VISUAL=nvim
 export EDITOR=nvim
 
-export PATH="$PATH:/usr/local/sbin:$DOTFILES/bin:$HOME/.local/bin:$DOTFILES/scripts/"
+export PATH="$PATH:$DOTFILES/scripts/"
 
-if type brew &>/dev/null
-then
-    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-    autoload -Uz compinit
-    compinit
-fi
+# Keybindings
+bindkey -v
+bindkey '^p' history-search-backward
+bindkey '^y' autosuggest-accept
+bindkey '^n' history-search-forward
+bindkey '^[w' kill-region
+bindkey -s '^f' "tmux_sessionizer\n"
 
-export NVM_DIR="/Users/dodi/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
 
-export DOTNET_ROOT=/usr/local/share/dotnet/x64
-export PATH=$PATH:$DOTNET_ROOT
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 
-export MONO_BIN=/Library/Frameworks/Mono.framework/Versions/6.12.0/bin
-export PATH=$PATH:$MONO_BIN
+# Aliases
+alias pn=pnpm
+alias love="/Applications/love.app/Contents/MacOS/love"
+alias gu="gitui"
+alias gf="git fetch"
+alias gs="git status"
+alias gvim="nvim --listen 127.0.0.1:55432"
+alias b="cd .."
+alias c=clear
+alias ls=eza
+alias lt="eza --tree --long"
+alias ll="eza --long"
+alias v=nvim
+alias n=nvim
+alias cat=bat
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
+# Switch branch with fuzzy finder
+alias gl="git log --all --decorate --graph --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
+alias lg="lazygit"
 
-# heroku autocomplete setup
-HEROKU_AC_ZSH_SETUP_PATH=/Users/dodi/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
 
-export ANDROID_SDK="$HOME/Library/Android/sdk"
-export PATH="$ANDROID_SDK/platform-tools:$PATH"
+take() {
+    mkdir -p $1
+    cd $1
+}
 
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+# tmux
+tma() {
+    tmux attach -t $1
+}
 
-# pnpm
-export PNPM_HOME="/Users/dodi/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-# pnpm end
+tmn() {
+    tmux new -s $1
+}
 
-# llvm
-export PATH=/opt/homebrew/opt/llvm/bin:$PATH
-
-# golang
-export PATH=$HOME/go/bin:$PATH
-
-# bun completions
-[ -s "/Users/dodi/.bun/_bun" ] && source "/Users/dodi/.bun/_bun"
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
-export PATH=$PATH:/Users/dodi/.spicetify
-
-# AWS config
-export AWS_SDK_LOAD_CONFIG=1
-export AWS_PROFILE=sdk-dev-developer
-export PASSPHRASE=Intekplus
-
-eval "$(lua ~/scripts/z.lua/z.lua --init zsh enhanced once fzf)"
-
-export PATH="/opt/homebrew/opt/icu4c/bin:$PATH"
-export PATH="/opt/homebrew/opt/icu4c/sbin:$PATH"
-
-eval "$(starship init zsh)"
-
-source_if_exists $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-source_if_exists $DOTFILES/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source_if_exists $DOTFILES/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-source /Users/dodi/.config/broot/launcher/bash/br
-
-export PATH="$PATH:/Users/dodi/.aiken/bin"
-
-# opam configuration
-[[ ! -r /Users/dodi/.opam/opam-init/init.zsh ]] || source /Users/dodi/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
-
-# ---- FZF -----
-# Set up fzf key bindings and fuzzy completion
+# Shell integrations
 eval "$(fzf --zsh)"
-# ---- END FZF -----
-
-PATH=~/.console-ninja/.bin:$PATH
-
-export GOPATH="$HOME/go"; export GOROOT="$HOME/.go"; export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
-alias gvm="$GOPATH/bin/g"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
+eval "$(zoxide init --cmd cd zsh)"
+eval $(thefuck --alias f)
