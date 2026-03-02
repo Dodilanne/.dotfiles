84050f34311c57399c810c4d1a0634f5c5df5ea7
Dodilanne <vincaudet@gmail.com>
2026-03-02 10:06:12 -0500

    Add obs triggers

--- skhd/rc.sh
+++ skhd/rc.sh
@@ -1,19 +1,15 @@
+alt + shift + ctrl - t : open -a "Ghostty"; obsws-cli scene switch Ghostty
+alt + shift + ctrl - c : open -a "Google Chrome"; obsws-cli scene switch Chrome
 alt + shift + ctrl - w : open -a "Finder"
 alt + shift + ctrl - f : open -a "Spotify"
 alt + shift + ctrl - p : open -a "Preview"
 alt + shift + ctrl - b : open -a "HTTPie"
-
-alt + shift + ctrl - a : osascript -e 'tell application "System Events" to set frontmost of first process whose name is "qemu-system-aarch64" to true'
-alt + shift + ctrl - r : open -a "hodei"
-alt + shift + ctrl - s : open -a "Discord"
-alt + shift + ctrl - t : open -a "Ghostty"
-alt + shift + ctrl - g : open -a "GitUp"
-
+alt + shift + ctrl - g : open -a "GitUp"; obsws-cli scene switch GitUp
 alt + shift + ctrl - z : open -a "Messages"
-alt + shift + ctrl - x : open -a "Google Chrome"
-alt + shift + ctrl - c : open -a "Google Chrome"
-alt + shift + ctrl - d : open -a "DataGrip"
+alt + shift + ctrl - d : open -a "DataGrip"; obsws-cli scene switch DataGrip
 alt + shift + ctrl - v : open -a "Preview"
-
 alt + shift + ctrl - m : open -a "Mail"
-alt + shift + ctrl - n : open -a "Xournal++"
+alt + shift + ctrl - n : open -a "Xournal++"; obsws-cli scene switch Xournal++
+alt + shift + ctrl - a : osascript -e 'tell application "System Events" to set frontmost of first process whose name is "qemu-system-aarch64" to true'
+alt + shift + ctrl - r : open -a "hodei"; obsws-cli scene switch Hodei
+alt + shift + ctrl - s : open -a "Discord"
